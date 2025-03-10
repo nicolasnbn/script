d99919ab95788526de6c92920051351c738a9bfc
Nicolas B <nicolasnbn@gmail.com>
2025-03-10 18:04:32 +0100

    Create nico.sh

---
+++ nico.sh
@@ -0,0 +1,53 @@
+#NICO
+
+#####################################################
+#POUR PYTHON
+
+ve(){
+    if [ -d "./venv/" ]; then
+        echo "le virtual env existe deja.";
+    else
+        echo "creation du virtual env.";
+        python -m venv venv;
+    fi
+    src;
+    if [ -f requirements.txt ]; then
+        echo "installation des modules du fichier de requirements.";
+        pip install -r requirements.txt;
+    fi
+}
+alias rmve='rm -r venv';
+alias src='source ./venv/bin/activate';
+alias srcd='deactivate';
+
+#####################################################
+export PATH="$HOME/tools:$PATH"
+
+#####################################################
+alias l='ls -lart';
+alias rz='. ~/tools/rezetnet.sh';
+alias cl="clear";
+alias hi="history | grep ";
+
+#####################################################
+# POUR JEDHA CLI
+
+alias js='jedha-cli start '
+alias jss='jedha-cli status'
+alias jsstatus='jedha-cli status | tail -n1 | cut -d ":" -f2|cut -d"." -f1'
+alias jst='jedha-cli stop $(jsstatus)'
+
+
+##############################
+## EXECUTE
+#
+
+if ! declare -f | grep -q "cln" ; then 
+    cln(){
+        awk '/#NICO/{print NR}' ~/.bashrc | (read THEVALUE && sed -i "${THEVALUE},\$d" ~/.bashrc);
+        awk '/#NICO/{print NR}' ~/.zshrc | (read THEVALUE && sed -i "${THEVALUE},\$d" ~/.zshrc);
+    }
+fi
+cln;
+cat /tmp/nico.sh >> ~/.bashrc &&  cat /tmp/nico.sh >> ~/.zshrc ;
+. ~/.zshrc 
